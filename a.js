118cd180da3160011ef1b916074b8d5e29125a0b
Bikash Dash <123199765+bikashdasha@users.noreply.github.com>
2026-04-05 20:26:44 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,20 +1,3 @@
-a.js‎
-+5
--1
-Lines changed: 5 additions & 1 deletion
-Original file line number	Diff line number	Diff line change
 function getData(arr){
-Comment on line R1
-bikashdasha commented now
-@bikashdasha
-bikashdasha
-now
-Owner
-Author
-This function can be optimized using map() instead of loop.
-
-Write a reply
-  for(let i=0;i<arr.length;i++){
-    console.log(arr[i]);
-  }
+  arr.forEach(item => console.log(item));
 }
